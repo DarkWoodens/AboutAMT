9085d15c6a202a77f38d99df48b8d199f4befa3c
DarkWoodens <paulionut2003@gmail.com>
2017-02-09 15:48:59 +0200

    1.0

---
+++ app/src/main/java/amt_team/center/amtabout/MainFragment.java
@@ -0,0 +1,24 @@
+package amt_team.center.amtabout;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.app.FragmentActivity;
+
+public class MainFragment extends android.support.v4.app.Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+
+}
